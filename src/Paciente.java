bae26fbb82a6361f1827c9858a5d1fff8142c39c
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2020-03-26 12:01:43 -0600

    First Push

---
+++ src/Paciente.java
@@ -0,0 +1,90 @@
+/**
+ * @author Jose Abraham Gutierrez C., 19111
+ * @date 01/04/2020
+ */
+public class Paciente implements Comparable<Paciente> {
+    // Atributos utilizados
+    private String name;
+    private String description;
+    private String code;
+
+    /**
+     *Constructor
+     * @param name nombre del paciente
+     * @param description descripcion
+     * @param code codigo del paciente
+     */
+    public Paciente (String name, String description, String code) {
+        this.name = name;
+        this.description = description;
+        this.code = code;
+    }
+
+    /**
+     * Este metodo edita el toString
+     * @return un STring ordenado
+     */
+    @Override
+    public String toString() {
+
+        return this.getNombre() + " |  " + this.getDescription() + " | " + this.getCode();
+    }
+
+    /**
+     * Compara los codigos de emergencia
+     * @param p un paciente
+     * @return un int
+     */
+    @Override
+    public int compareTo(Paciente p) {
+        return this.getCode().compareTo(p.getCode());
+    }
+
+    /**
+     * Se hace un get del nombre del paciente
+     * @return un String
+     */
+    public String getNombre() {
+        return name;
+    }
+    /**
+     * Se hace un set del nombre
+     * @param name un string con el nombre
+     */
+    public void setNombre(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Se obtiene el codigo
+     * @return un string
+     */
+    public String getCode() {
+        return code;
+    }
+
+
+    /***
+     * Se le hace un get a la descripcion
+     * @return string
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Se le hace set a la descripcion
+     * @param description un string
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Se le hace un set al codde
+     * @param code es un string
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
---
+++ src/PriorityQueue.java
@@ -0,0 +1,51 @@
+/**
+ * @author Jose Abraham Gutierrez C., 19111
+ * @date 01/04/2020
+ */
+public interface PriorityQueue<E extends Comparable<E>> {
+
+    /**
+     * Metodo getFirst
+     * @return el valor minimo en Priority queue
+     */
+    public E getFirst();
+    // pre: !isEmpty()
+    // post: returns the minimum value in priority queue
+
+    /**
+     * Metodo Remove
+     * @return y remueve el valor minimo de queue
+     */
+    public E remove();
+    // pre: !isEmpty()
+    // post: returns and removes minimum value from queue
+
+    /**
+     * Metodo add
+     * @param value se agrega a Priority queue
+     */
+    public void add(E value);
+    // pre: value is non-null comparable
+    // post: value is added to priority queue
+
+    /**
+     * Metodo si esta vacio
+     * @return si no hay elementos en queue
+     */
+    public boolean isEmpty();
+    // post: returns true iff no elements are in queue
+
+    /**
+     * Metodo size
+     * @return el numero de elementos en queue
+     */
+    public int size();
+    // post: returns number of elements within queue
+
+    /**
+     * Metodo clear
+     * elimina los elementos de queue
+     */
+    public void clear();
+    // post: removes all elements from queue
+}
